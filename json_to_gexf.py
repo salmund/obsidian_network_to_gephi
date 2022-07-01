eb70342a1d66933394d92d13498ae7b0c41ee6fc
Louis <105465034+salmund@users.noreply.github.com>
2022-07-01 22:54:46 +0200

    Update json_to_gexf.py

--- json_to_gexf.py
+++ json_to_gexf.py
@@ -1,16 +1,30 @@
 import networkx as nx
 import json
 
-g = nx.Graph()
+import argparse
 
-with open('my graph.json') as f:
-    data = json.load(f)
+parser = argparse.ArgumentParser(description='Convert your JSON Obsidian Network to GEXF.')
 
-for node, neighbors in data.items():
-    name = node.split("/")[-1].replace(".md", "")
-    g.add_node(name)
-    for neighbor in neighbors:
-        n_name = neighbor.split("/")[-1].replace(".md", "")
-        g.add_edge(name, n_name)
 
-nx.write_gexf(g, "graph.gexf")
+parser.add_argument("file", type=str, help="specify the JSON file.", default="No file specified.")
+
+args = parser.parse_args()
+
+def export_to_gexf(file):
+    g = nx.Graph()
+
+    with open(file) as f:
+        data = json.load(f)
+
+    for node, neighbors in data.items():
+        name = node.split("/")[-1].replace(".md", "")
+        g.add_node(name)
+        for neighbor in neighbors:
+            n_name = neighbor.split("/")[-1].replace(".md", "")
+            g.add_edge(name, n_name)
+
+    nx.write_gexf(g, "graph.gexf")
+
+if __name__ == '__main__':
+    export_to_gexf(args.file)
+
